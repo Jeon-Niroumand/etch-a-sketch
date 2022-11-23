5213971723bc25163ca538604234bd83caaf93e2
Jeon Niroumand <jeon.niroumand@gmail.com>
2022-11-22 21:27:31 -0800

    started method for drawing grid in js

---
+++ eas.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector('#grid');
+
+const box = document.createElement('div');//make a box
+box.setAttribute('style', 'width: 25px; height: 25px; border-style: solid; background: pink;');
+grid.appendChild(box);
+
+const box1 = document.createElement('div');//make a box
+box1.setAttribute('style', 'width: 25px; height: 25px; border-style: solid; background: pink;');
+grid.appendChild(box1);
+
+
+
